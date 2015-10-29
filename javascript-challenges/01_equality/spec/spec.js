d24cae1e22671cf4651a433bd5c7d0339ec35c09
davidtadams <adams.t.david@gmail.com>
2015-10-29 16:12:19 -0600

    finished exercise 01 on equality

--- javascript-challenges/01_equality/spec/spec.js
+++ javascript-challenges/01_equality/spec/spec.js
@@ -11,48 +11,48 @@ describe('sameLength', function() {
 });
 
 describe('firstGreater', function() {
-  xit('returns true if the first argument is (strictly) greater than the second argument', function() {
+  it('returns true if the first argument is (strictly) greater than the second argument', function() {
     expect(yourCode.firstGreater('a', 'A')).toEqual(true);
     expect(yourCode.firstGreater(3, -1)).toEqual(true);
   });
 
-  xit('returns false if the first argument is less than or equal to the second argument', function() {
+  it('returns false if the first argument is less than or equal to the second argument', function() {
     expect(yourCode.firstGreater('a', 'a')).toEqual(false);
     expect(yourCode.firstGreater(1, 1)).toEqual(false);
   });
 });
 
 describe('firstGreaterOrEqualTo', function() {
-  xit('returns true if the first argument is greater than or equal to the second argument', function() {
+  it('returns true if the first argument is greater than or equal to the second argument', function() {
     expect(yourCode.firstGreaterOrEqualTo('a', 'A')).toEqual(true);
     expect(yourCode.firstGreaterOrEqualTo(1, 2)).toEqual(false);
   });
 
-  xit('returns false if the first argument is (strictly) less than the second argument', function() {
+  it('returns false if the first argument is (strictly) less than the second argument', function() {
     expect(yourCode.firstGreaterOrEqualTo('a', 'b')).toEqual(false);
     expect(yourCode.firstGreaterOrEqualTo(3, -1)).toEqual(true);
   });
 });
 
 describe('firstLengthGreater', function() {
-  xit('returns true if the first argument is (strictly) longer than the second argument\'s length', function() {
+  it('returns true if the first argument is (strictly) longer than the second argument\'s length', function() {
     expect(yourCode.firstLengthGreater('aa', 'A')).toEqual(true);
     expect(yourCode.firstLengthGreater([1,2,3], [1])).toEqual(true);
   });
 
-  xit('returns false if the first argument is less than or equal to the second argument\'s length', function() {
+  it('returns false if the first argument is less than or equal to the second argument\'s length', function() {
     expect(yourCode.firstLengthGreater('a', 'a')).toEqual(false);
     expect(yourCode.firstLengthGreater([1,2,3], [2,3,4])).toEqual(false);
   });
 });
 
 describe('secondGreater', function() {
-  xit('returns true if the second argument is (strictly) greater than the first argument', function() {
+  it('returns true if the second argument is (strictly) greater than the first argument', function() {
     expect(yourCode.secondGreater('a', 'A')).toEqual(false);
     expect(yourCode.secondGreater(-1, 3)).toEqual(true);
   });
 
-  xit('returns false if the second argument is less than or equal to the first argument', function() {
+  it('returns false if the second argument is less than or equal to the first argument', function() {
     expect(yourCode.secondGreater('a', 'a')).toEqual(false);
     expect(yourCode.secondGreater(1, 1)).toEqual(false);
   });
--- javascript-challenges/01_equality/src.js
+++ javascript-challenges/01_equality/src.js
@@ -1,5 +1,21 @@
 module.exports = {
   sameLength: function(string1, string2) {
-    // your code here:
+    return string1.length === string2.length;
+  },
+
+  firstGreater: function(arg1, arg2){
+    return arg1 > arg2;
+  },
+
+  firstGreaterOrEqualTo: function(arg1, arg2){
+    return arg1 >= arg2;
+  },
+
+  firstLengthGreater: function(arg1, arg2){
+    return arg1.length > arg2.length;
+  },
+
+  secondGreater: function(arg1, arg2) {
+    return arg2 > arg1;
   }
 }
