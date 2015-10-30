113aa4f1957bf14ef34b4bf344153effc4203bfa
davidtadams <adams.t.david@gmail.com>
2015-10-30 10:31:26 -0600

    working on exercise 03

--- javascript-challenges/03_arrays/src.js
+++ javascript-challenges/03_arrays/src.js
@@ -1,7 +1,34 @@
 module.exports = {
 
   first: function(array) {
-    // fill in code here
-  }
+    return array[0];
+  },
 
+  last: function(array) {
+    return array[array.length - 1];
+  },
+
+  empty: function(array) {
+    return !array.length
+  },
+
+  first_n: function(array, num) {
+    
+  },
+
+  last_n: function(array) {
+
+  },
+
+  drop: function(array) {
+
+  },
+
+  union: function(array) {
+
+  },
+
+  intersection: function(array) {
+
+  }
 }
