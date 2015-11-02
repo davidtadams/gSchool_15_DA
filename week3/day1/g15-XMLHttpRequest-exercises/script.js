627a91ddf8959be4b2487b93e96a3f4c0cf6f7db
davidtadams <adams.t.david@gmail.com>
2015-11-02 11:54:59 -0700

    finished all three ajax request exercises

--- week3/day1/g15-XMLHttpRequest-exercises/script.js
+++ week3/day1/g15-XMLHttpRequest-exercises/script.js
@@ -1,13 +1,40 @@
-console.log('You\'re ready to begin!')
+/* First request to request index.html */
+var request1 = new XMLHttpRequest();
 
-var request = new XMLHttpRequest()
-
-request.onreadystatechange = function () {
+request1.onreadystatechange = function () {
   if (this.readyState === 4 && this.status == 200) {
-    console.log(this.responseText)
+    console.log(this.responseText);
   }
 }
 
-request.open('GET', '/index.html')
+request1.open('GET', './index.html');
+request1.send();
 
-request.send()
+/* Second request to request json data */
+var request2 = new XMLHttpRequest();
+
+request2.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status == 200) {
+    var data = JSON.parse(this.responseText);
+    console.log(JSON.stringify(data));
+  }
+}
+
+request2.open('GET', './data.json');
+request2.send();
+
+/* Third request to Reddit JSON API */
+var request3 = new XMLHttpRequest();
+
+request3.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status == 200) {
+    listings = JSON.parse(this.responseText);
+    for (var i = 0; i < listings.data.children.length; i++) {
+      var listing = listings.data.children[i];
+      console.log(listing.data.title, listing.data.url, listing.data.author);
+    }
+  }
+}
+
+request3.open('GET', 'http://www.reddit.com/r/GetMotivated.json');
+request3.send();
