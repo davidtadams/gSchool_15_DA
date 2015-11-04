e54e7eb05e906f4e68af6b2c7cbf8a0b7570cbf4
davidtadams <adams.t.david@gmail.com>
2015-11-04 11:45:47 -0700

    adding warmup file to manipulate the verge website

---
+++ week3/day3/warmup.js
@@ -0,0 +1,7 @@
+var article = document.getElementsByClassName("m-hero__slot-link")[0];
+var articleDiv = article.getElementsByTagName('div')[0];
+var articleHead = article.getElementsByTagName('h2')[0];
+var articleAuth = article.getElementsByTagName('p')[0];
+articleHead.innerText = "Elon Musk moves to MARS! Finds love in a hopeless place";
+articleDiv.style.backgroundImage = "url('http://www.bloomberg.com/graphics/2015-elon-musk-spacex/img/musk_final.jpg')";
+articleAuth.innerText = "By David Adams";
