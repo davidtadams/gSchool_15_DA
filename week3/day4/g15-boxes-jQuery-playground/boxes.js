11ca49fe5bf9a0ccb64265181571af4f0971fe14
davidtadams <adams.t.david@gmail.com>
2015-11-05 17:20:27 -0700

    finished exercise

--- week3/day4/g15-boxes-jQuery-playground/boxes.js
+++ week3/day4/g15-boxes-jQuery-playground/boxes.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   /* Exercise 1 - DOM Ready */
   //alert to show when DOM is done loading
-  // alert('Ready for DOM Maniuplation');
+  alert('Ready for DOM Maniuplation');
 
   /* Exercise 2 - Selectors, CSS , Attributes */
   //find secret box, set background->white add h1->secret box!
@@ -21,15 +21,44 @@ $(document).ready(function() {
   $('.boxType1').css("background", "white");
 
   //get first two divs in the second row, take away all styling
-  /* don't know if this means to just take away the color and leave box class or remove all classes
-    I did it both ways and commented out one.*/
-  // $('#row2 div:nth-child(-n+2)').removeClass();
-  $('#row2 div:nth-child(-n+2)').removeClass("boxType1 boxType2 boxType3");
+  $('#row2 div:nth-child(-n+2)').removeClass();
 
   //get all divs inside container that are not row divs and not secret box div. set width->2pixels
   $('.row div:not(#secretBox)').css("width", "2px");
 
   /* Exercise 3 - Events */
+  //Log the x and y position of a click event in the main container
+  $('#container').click(function(event) {
+    console.log(event.pageX, event.pageY);
+  });
 
+  //Number 3.2
+  $('.boxType1').append('<a href="www.galvanize.com" class="neverLeave">Galvanize</a>');
+  $('.neverLeave').click(function(event) {
+    event.preventDefault();
+    alert("You can never leave!!!!");
+  });
 
+  //Number 3.3
+  $('.box').click(function(event) {
+    if ($(this)[0].children.length === 1) {
+      $(this)[0].children[0].remove();
+    } else {
+      $(this).append('<img src="http://placecreature.com/puppy/100/100" alt="cutePuppy">');
+    }
+  });
+
+  //Number 3.4
+  var container = $('#container');
+  container.click(function(event) {
+    if (event.target == container[0]) {
+      //if original div == container div -> change background of container to lime green
+      container.css("background", "green");
+    } else {
+      //turn container background to black
+      //background of original div clicked to white
+      container.css("background", "black");
+      $(event.target).css("background", "white");
+    }
+  });
 })
