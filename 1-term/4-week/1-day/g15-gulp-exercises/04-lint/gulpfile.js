44908221780e7340dbaf56495037cd770b841e50
davidtadams <adams.t.david@gmail.com>
2015-11-09 13:53:07 -0700

    finished exercise 4

--- 1-term/4-week/1-day/g15-gulp-exercises/04-lint/gulpfile.js
+++ 1-term/4-week/1-day/g15-gulp-exercises/04-lint/gulpfile.js
@@ -1,3 +1,11 @@
 var gulp = require('gulp');
+var eslint = require('gulp-eslint');
 
-gulp.task('default');
+gulp.task('default', ['lint']);
+
+gulp.task('lint', function () {
+  return gulp.src('./src/**/*.js')
+      .pipe(eslint())
+      .pipe(eslint.format())
+      .pipe(eslint.failAfterError());
+})
--- 1-term/4-week/1-day/g15-gulp-exercises/04-lint/src/app.js
+++ 1-term/4-week/1-day/g15-gulp-exercises/04-lint/src/app.js
@@ -1,3 +1,3 @@
 var list = require('./components/list.js');
-console.log('I'm an app!')
+console.log('I\'m an app!');
 list();
