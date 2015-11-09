eeae1a2cbdab5cf24126fd59f20ec11292183d42
davidtadams <adams.t.david@gmail.com>
2015-11-09 13:40:32 -0700

    finished exercise 2

--- 1-term/4-week/1-day/g15-gulp-exercises/02-task/gulpfile.js
+++ 1-term/4-week/1-day/g15-gulp-exercises/02-task/gulpfile.js
@@ -1,3 +1,12 @@
 var gulp = require('gulp');
 
-gulp.task('default')
+gulp.task('default', ['delayed'], function () {
+  console.log('running default task');
+});
+
+gulp.task('delayed', function (done) {
+  setTimeout(function () {
+    console.log('delayed by 2 seconds');
+    done();
+  }, 2000);
+});
