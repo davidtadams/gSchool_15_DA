2c49723dfc7421df4c3e2c04ec1dcfb4e36e1aba
davidtadams <adams.t.david@gmail.com>
2015-11-11 14:12:19 -0700

    finished local storage exercise

--- 1-term/4-week/3-day/g15-localstorage-exercises/03-json/script.js
+++ 1-term/4-week/3-day/g15-localstorage-exercises/03-json/script.js
@@ -2,6 +2,10 @@ var data = {
   title: "My favorite data",
   submittedBy: "Fred",
   points: 1337
-}
+};
 
 // Your code here
+localStorage.setItem('data', JSON.stringify(data));
+var getData = JSON.parse(localStorage.getItem('data'));
+getData.title = "My NEW favorite title!";
+localStorage.setItem('data', JSON.stringify(getData));
--- 1-term/4-week/3-day/g15-localstorage-exercises/04-cached-response/script.js
+++ 1-term/4-week/3-day/g15-localstorage-exercises/04-cached-response/script.js
@@ -1,7 +1,10 @@
 $.get('https://www.reddit.com/r/aww.json')
   .done(function (response) {
     //What to do if HTTP was successful
+    localStorage.setItem('redditData', JSON.stringify(response));
   })
   .fail(function (error) {
     //What to do if HTTP failed
-  })
+    var redditData = JSON.parse(localStorage.getItem('redditData'));
+    console.log('Offline cache data: ', redditData);
+  });
