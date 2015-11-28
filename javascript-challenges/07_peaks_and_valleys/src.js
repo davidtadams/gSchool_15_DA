0bc78502f5c36adfa636d9fddb96f4b32c78148e
davidtadams <adams.t.david@gmail.com>
2015-11-28 14:20:11 -0600

    finished exercise 07 peaks and valleys

--- javascript-challenges/07_peaks_and_valleys/src.js
+++ javascript-challenges/07_peaks_and_valleys/src.js
@@ -11,5 +11,38 @@
 module.exports = {
   peak: function() {
     // your code here
+    return '/\\';
+  },
+
+  valley: function() {
+    return '___';
+  },
+
+  peaks: function(num) {
+    var retStr = "";
+    for (var i = 0; i < num; i++) {
+      retStr += this.peak();
+    }
+    return retStr;
+  },
+
+  valleys: function(num) {
+    var retStr = "";
+    for (var i = 0; i < num; i++) {
+      retStr += this.valley();
+    }
+    return retStr;
+  },
+
+  peaksAndValleys: function(peaks, valleys) {
+    var retStr = "";
+    for (var i = 0; i < peaks; i++) {
+      retStr += this.peak();
+    }
+    for (var i = 0; i < valleys; i++) {
+      retStr += this.valley();
+    }
+    return retStr;
   }
+
 }
