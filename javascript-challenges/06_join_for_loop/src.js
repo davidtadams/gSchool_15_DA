5319a1e030f0e7f32252ad420390a98283c8df68
davidtadams <adams.t.david@gmail.com>
2015-11-28 14:08:50 -0600

    finished exercise 06 joins with for loops

--- javascript-challenges/06_join_for_loop/src.js
+++ javascript-challenges/06_join_for_loop/src.js
@@ -6,6 +6,59 @@ module.exports = {
 
   joinWithFor: function(list) {
     // your code here
-  }
+    var retStr = "";
+    for (var i = 0; i < list.length; i++) {
+      retStr += list[i];
+    }
+    return retStr;
+  },
 
+  joinWithForAndIndex: function(list) {
+    var retStr = "";
+    for (var i = 0; i < list.length; i++) {
+      retStr += list[i] + i;
+    }
+    return retStr;
+  },
+
+  joinWithoutOddCharacters: function(list) {
+    var retStr = "";
+    for (var i = 0; i < list.length; i++) {
+      if (i % 2 === 0) {
+        retStr += list[i];
+      }
+    }
+    return retStr;
+  },
+
+  joinWithForAndToken: function(list) {
+    var retStr = "";
+    for (var i = 0; i < list.length; i++) {
+      if (i === list.length - 1) {
+        retStr += list[i];
+      }
+      else {
+        retStr += list[i] + '*';
+      }
+    }
+    return retStr;
+  },
+
+  joinWithForAndAlternatingTokens: function(list) {
+    var retStr = "";
+    for (var i = 0; i < list.length; i++) {
+      if (i === list.length - 1) {
+        retStr += list[i];
+      }
+      else {
+        if (i % 2 === 0) {
+          retStr += list[i] + '*';
+        }
+        else {
+          retStr += list[i] + '+';
+        }
+      }
+    }
+    return retStr;
+  }
 }
