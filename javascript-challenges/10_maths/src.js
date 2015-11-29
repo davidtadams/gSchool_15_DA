feb3a6cd1310fcf0e526b36f7c1e191d10f37942
davidtadams <adams.t.david@gmail.com>
2015-11-29 16:32:23 -0600

    finished exercise 10 on maths

--- javascript-challenges/10_maths/src.js
+++ javascript-challenges/10_maths/src.js
@@ -4,26 +4,91 @@
 
 module.exports = {
   maximum: function(array) {
+    var max = array[0];
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] > max) {
+        max = array[i];
+        arrIndex = i;
+      }
+    }
+    return max;
   },
 
   minimum: function(array) {
+    var min = array[0];
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] < min) {
+        min = array[i];
+      }
+    }
+    return min;
   },
 
   average: function(arrayOfNumbers) {
+    var sum = 0;
+    for (var i = 0; i < arrayOfNumbers.length; i++) {
+      sum += arrayOfNumbers[i];
+    }
+    return sum / arrayOfNumbers.length;
   },
 
   matrixRowMaximum: function(arrayOfArrays) {
+    var retArr = [];
+    var max = 0;
+    for (var i = 0; i < arrayOfArrays.length; i++) {
+      max = arrayOfArrays[i][0];
+      for (var j = 0; j < arrayOfArrays[i].length; j++) {
+        if (arrayOfArrays[i][j] > max) {
+          max = arrayOfArrays[i][j];
+        }
+      }
+      retArr.push(max);
+    }
+    return retArr;
   },
 
   matrixRowMinimum: function(arrayOfArrays) {
+    var retArr = [];
+    var min = 0;
+    for (var i = 0; i < arrayOfArrays.length; i++) {
+      min = arrayOfArrays[i][0];
+      for (var j = 0; j < arrayOfArrays[i].length; j++) {
+        if (arrayOfArrays[i][j] < min) {
+          min = arrayOfArrays[i][j];
+        }
+      }
+      retArr.push(min);
+    }
+    return retArr;
   },
 
   matrixRowAverage: function(arrayOfArrays) {
+    var retArr = [];
+    var sum;
+    for (var i = 0; i < arrayOfArrays.length; i++) {
+      sum = 0;
+      for (var j = 0; j < arrayOfArrays[i].length; j++) {
+        sum += arrayOfArrays[i][j];
+      }
+      retArr.push(sum / arrayOfArrays[i].length);
+    }
+    return retArr;
   },
 
   sortedRowMatrix: function(arrayOfArrays) {
+    var retArr = [];
+    for (var i = 0; i < arrayOfArrays.length; i++) {
+      retArr.push(arrayOfArrays[i].sort());
+    }
+    return retArr;
   },
 
   lengthOfWords: function(sentence) {
+    var retArr = [];
+    var splitSentence = sentence.split(" ");
+    for (var i = 0; i < splitSentence.length; i++) {
+      retArr.push(splitSentence[i].length);
+    }
+    return retArr;
   }
 }
