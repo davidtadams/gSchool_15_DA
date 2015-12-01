e00926acf43c9f6d7fb330303573a0af86207343
davidtadams <adams.t.david@gmail.com>
2015-12-01 16:11:05 -0700

    adding solutins to daily code challenge

---
+++ 2-term/1-week/2-day/dailyChallenge.md
@@ -0,0 +1,27 @@
+# Challenge 1:  Count It
+count the letters in a string.
+
+## Input
+A string - like "Hello World"
+
+## Output
+Letters and how often they show up. - d:1 e:1 h:1 l:3 o:2 r:1 w:1
+
+## Special
+convert all to lowercase. Ignore whitespace and anything not a-z
+
+## Challenge input:
+"The quick brown fox jumps over the lazy dog and the sleeping cat early in the day."
+
+[Source](https://www.reddit.com/r/dailyprogrammer/comments/2mkh5g/weekly_17_mini_challenges/cm51y55)
+
+<hr>
+
+# Challenge 2: Ramp Numbers
+A ramp number is a number whose digits from left to right either only rise or stay the same. 1234 is a ramp number as is 1124. 1032 is not.
+
+## Input
+A positive integer, n.
+
+## Output
+true if the input is a ramp number, false if it is not
---
+++ 2-term/1-week/2-day/dailyCodingChallenge.js
@@ -0,0 +1,114 @@
+/* Challenge #1 */
+function countLetters(str) {
+  var results = {};
+  str = str.toLowerCase();
+  for (var i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) >= 97 &&  str.charCodeAt(i) <= 122) {
+      if (results[str[i]] > 0) {
+        results[str[i]] += 1;
+      }
+      else {
+        results[str[i]] = 1;
+      }
+    }
+  }
+
+  for (var i in results) {
+    process.stdout.write(i + ':' + results[i] + ' ');
+  }
+
+  /* another way to print it out */
+  console.log('\n' + JSON.stringify(results));
+}
+
+console.log('Testing the countLetters function:');
+console.log('Answer for string "Hello World"');
+countLetters("Hello World");
+console.log('\n');
+console.log('Answer for sring quick brown fox');
+countLetters("The quick brown fox jumps over the lazy dog and the sleeping cat early in the day.");
+console.log('\n');
+
+/* Challenge #2 */
+function rampNumber(num) {
+  numStr = num.toString();
+  for (var i = 0; i < numStr; i++) {
+    if (i != 0) {
+      if (numStr[i - 1] > numStr[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+console.log('Testing the function rampNumber:');
+console.log(rampNumber(1234));
+console.log(rampNumber(1124));
+console.log(rampNumber(1032));
+console.log(rampNumber(111111101));
+console.log(rampNumber(13579));
+console.log(rampNumber(135792));
+
+
+/* ----------------------------------------------------------------------------
+                                  SOLUTIONS
+------------------------------------------------------------------------------*/
+/* Danny's solution */
+// function countLetters (input) {
+// 	return printPaths(sanitize(input).split('').reduce(incrementPath, {}));
+// }
+//
+// function incrementPath (object, path) {
+// 	if (!object[path]) {
+// 		object[path] = 0;
+// 	}
+// 	object[path] += 1;
+// 	return object;
+// }
+//
+// function sanitize (input) {
+// 	return input.toLowerCase().replace(/[^a-z]/ig, '');
+// }
+//
+// function formatLetter (letter, count) {
+// 	return letter + ':' + count;
+// }
+//
+// function printPaths (object) {
+// 	return Object.keys(object).map(function (key) {
+// 		return formatLetter(key, object[key]);
+// 	}).join(' ');
+// }
+//
+// console.log(countLetters("Hello World"));
+//
+// /* CJ's solution */
+// function countLetters(input) {
+//     // a place to store the counters
+//     var counters = {};
+//
+//     // convert input to lowercase
+//     input = input.toLowerCase();
+//
+//     // iterate over the chars of the string
+//     for (var i = 0; i < input.length; i++) {
+//         // check to see if a letter
+//         if(input[i] <= 'z' && input[i] >= 'a') {
+//             // convert the char to lowercase - str.toLowerCase();
+//             if(counters.hasOwnProperty(input[i])) {
+//                 // increment counter for given char obj[char]++
+//                 counters[input[i]]++;
+//             }
+//             else {
+//                 counters[input[i]] = 1;
+//             }
+//         }
+//     }
+//
+//     // print out the char counts
+//     return JSON.stringify(counters);
+// }
+//
+// var result = countLetters('The quick brown fox jumps over the lazy dog and the sleeping cat early in the day.');
+// console.log(result);
