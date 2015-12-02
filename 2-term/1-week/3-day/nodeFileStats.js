794fc893e94a6c3ba148c895ebb77b5740d10550
davidtadams <adams.t.david@gmail.com>
2015-12-02 13:51:41 -0700

    adding version with promises

--- 2-term/1-week/3-day/nodeFileStats.js
+++ 2-term/1-week/3-day/nodeFileStats.js
@@ -20,3 +20,45 @@ function printStats (stats) {
   var modified_time = moment(stats.mtime).format('YYYY MMM DD');
   console.log('modified time:', modified_time);
 }
+
+
+/* Danny's version with promises */
+// var readline = require('readline')
+// var fs = require('fs')
+// var moment = require('moment')
+​
+statP('Danny.json')
+.then(function (stats) {
+  return writeP('test.txt', JSON.stringify(stats))
+})
+.then(function (filename) {
+  console.log('written to', filename)
+})
+.catch(function (error) {
+  console.log(error)
+})
+​
+//Changing it from callback form to promise form
+function statP (filename) {
+  return new Promise(function (resolve, reject) {
+    fs.stat(filename, function (err, stats) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(stats)
+      }
+    })
+  })
+}
+​
+function writeP (filename, content) {
+  return new Promise(function (resolve, reject) {
+    fs.writeFile(filename, content, function (err) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(filename)
+      }
+    })
+  })
+}
