13360f2f63bd2ddf14ee3529c83d1ab1187cdd50
davidtadams <adams.t.david@gmail.com>
2015-12-05 12:21:10 -0700

    finished nodejs server mini project

--- 2-term/1-week/5-day/mini-project/server.js
+++ 2-term/1-week/5-day/mini-project/server.js
@@ -8,18 +8,44 @@ Example: http://localhost:1337/index.html?from=hello&to=bye
 will change all instances of hello in index.html to bye
 
 */
-
 var http = require('http')
 var fs = require('fs')
+var url = require('url')
+var path = require('path')
+var querystring = require('querystring')
+var port = 8080
 
 //create response handler
-
-  //parse query string
+function handler(request, response) {
+  //parse query string url
+  var parsedUrl = url.parse(request.url)
+  var pathname = path.join('.', parsedUrl.pathname)
+  var parsedQueryString = querystring.parse(parsedUrl.query)
 
   //get file
+  fs.readFile(pathname, 'utf8', function(err, contents) {
+    if (err) {
+      response.statusCode = 404
+      response.end('Error: 404')
+      return
+    }
 
-  //modify file
+    //modify file
+    var regex = new RegExp(parsedQueryString.from, 'ig')
+    var replaceString = '<strong>' + parsedQueryString.to + '</strong>'
+    contents = contents.replace(regex, replaceString)
 
-  //return file
+    //return file
+    response.setHeader('Content-Type', 'text/html')
+    response.statusCode = 200
+    response.end(contents)
+  })
+}
 
 //create server
+var server = http.createServer(handler)
+
+//start server
+server.listen(port, function() {
+  console.log('Started listening on port:', port)
+})
