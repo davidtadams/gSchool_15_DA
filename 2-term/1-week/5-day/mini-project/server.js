90ed8c5428b6dd247c9d7da0a009216edf4f563a
davidtadams <adams.t.david@gmail.com>
2015-12-05 11:26:53 -0700

    starting mini project

--- 2-term/1-week/5-day/mini-project/server.js
+++ 2-term/1-week/5-day/mini-project/server.js
@@ -1,3 +1,14 @@
+/*
+Mini Project
+
+Create a Node HTTP server that serves up the requested file
+The server accepts 2 query parameters: from and to
+Before sending out the file, change all instance of from in the file to to
+Example: http://localhost:1337/index.html?from=hello&to=bye
+will change all instances of hello in index.html to bye
+
+*/
+
 var http = require('http')
 var fs = require('fs')
 
