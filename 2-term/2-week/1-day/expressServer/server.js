93a7ee45471122295410e15589ff9ccd1c0aa00e
davidtadams <adams.t.david@gmail.com>
2015-12-07 16:44:33 -0700

    adding code for stretch server of hitting someone elses server

---
+++ 2-term/2-week/1-day/expressServer/server.js
@@ -0,0 +1,58 @@
+var http = require('http')
+var Express = require('express')
+var serveStatic = require('serve-static')
+var cors = require('cors')
+var morgan = require('morgan')
+
+//https://www.npmjs.com/package/request
+var request = require('request')
+
+var app = Express()
+
+app.use(cors())
+app.use(morgan('combined'))
+
+app.use(serveStatic('./public', {'index': ['index.html', 'index.htm']}))
+
+app.get('/chris', function(req, res) {
+  //make http get request to chris' server
+  request('http://10.6.81.24:3000/', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body) // Show the HTML for the Google homepage.
+      res.send(body)
+    }
+    res.send('error')
+  })
+})
+
+app.get('/mike', function(req, res) {
+  //make http get request to chris' server
+  request('http://10.6.82.192:3333/info', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body) // Show the HTML for the Google homepage.
+      var data = JSON.parse(body);
+      var html = "<!DOCTYPE html><html><head><meta charset='utf-8'><title>David Server</title></head>";
+      html += '<body><ul><li>Name: ' + data.name + '</li>';
+      html += '<li>Age: ' + data.age + '</li>';
+      html += '<li>Height: ' + data.height + '</li></ul></body>'
+      html += '</html>';
+      res.send(html);
+      return;
+    }
+    res.send('error')
+  })
+})
+
+var me = {
+  name: 'David',
+  age: 27,
+  height: '5-10'
+}
+
+app.get('/info', function(req, res) {
+  res.send(me);
+})
+
+app.listen(3000, function(){
+  console.log('Starting a server on localhost:3000');
+})
