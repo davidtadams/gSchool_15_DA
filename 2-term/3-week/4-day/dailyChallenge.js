87efce699fd27a3485e7d28fc9667bd422f37426
davidtadams <adams.t.david@gmail.com>
2015-12-17 22:09:48 -0700

    adding answer to daily challenge 17

--- 2-term/3-week/4-day/dailyChallenge.js
+++ 2-term/3-week/4-day/dailyChallenge.js
@@ -1,64 +1,63 @@
-function table (string) {
-  var array = string.split('\n');
-  var i, j, z;
-
-  for(i = 0; i < array.length; i++) {
-    array[i] = array[i].split(',');
-  }
-
-  var max1 = 0;
-  var max2 = 0;
-  var max3 = 0;
-
-  for (i = 0; i < array.length; i++) {
-    for (j = 0; j < array[i].length; j++) {
-      if (j === 0) {
-        //column 1
-        if (array[i][j].length > max1) {
-          max1 = array[i][j].length;
-        }
-      } else if (j === 1) {
-        //column 2
-        if (array[i][j].length > max2) {
-          max2 = array[i][j].length;
-        }
-      } else {
-        //column 3
-        if (array[i][j].length > max3) {
-          max3 = array[i][j].length;
-        }
-      }
-    }
-  }
-
-  for (i = 0; i < array.length; i++) {
-    for (j = 0; j < array[i].length; j++) {
-      if (j === 0) {
-        //column 1
-        if (array[i][j].length < max1) {
-          console.log(max1 - array[i][j].length);
-          // for (z = 0; z < max1 - array[i][j].length - 1; z++) {
-          //   array[i][j] += ' ';
-          // }
-        }
-      } else if (j === 1) {
-        //column 2
-
-      } else {
-        //column 3
-
-      }
-    }
-  }
-
-
-  console.log(array);
-  array = array.join('\n');
-  console.log(array);
-
-  // console.log(string.replace(/,/g, ' | '));
-  // return true;
+function csvToMd(string) {
+  var stringArray = splitStringToArray(string);
+  var maxArray = getMaxColumnLength(stringArray);
+  stringArray = padColumns(stringArray, maxArray);
+  return joinArrayToString(stringArray);
 }
 
-var csv = "First,Last,Address\nAnnamarie,Romaguera,9446 Schroeder Squares\nSebastian,Hessel,114 Frederic Centers\nMarlon,Hahn,307 Bradtke Grove\nDuane,Schowalter,10771 Keaton Knoll\nLaurence,Schuster,610 Odell Point"
-console.log(table(csv));
+function splitStringToArray(string) {
+  var stringArray = [];
+  string = string.split('\n');
+  for (var i = 0; i < string.length; i++) {
+    if (i === 1) {
+      stringArray.push([]);
+    }
+    stringArray.push(string[i].split(','));
+  }
+  return stringArray;
+}
+
+function getMaxColumnLength(stringArray) {
+  var maxArray = [];
+
+  for (var a = 0; a < stringArray[0].length; a++) {
+    maxArray.push(0);
+  }
+
+  for (var i = 0; i < stringArray.length; i++) {
+    for (var j = 0; j < stringArray[0].length; j++) {
+      if (i != 1) {
+        if (maxArray[j] < stringArray[i][j].length) {
+          maxArray[j] = stringArray[i][j].length;
+        }
+      }
+    }
+  }
+
+  return maxArray;
+}
+
+function padColumns(stringArray, maxArray) {
+  for (var i = 0; i < stringArray.length; i++) {
+    for (var j = 0; j < stringArray[0].length; j++) {
+      if (i === 1) {
+        stringArray[i][j] = '-'.repeat(maxArray[j]);
+      }
+      stringArray[i][j] += ' '.repeat(maxArray[j] - stringArray[i][j].length);
+    }
+  }
+  return stringArray;
+}
+
+function joinArrayToString(stringArray) {
+  for (var i = 0; i < stringArray.length; i++) {
+    stringArray[i] = stringArray[i].join(' | ');
+  }
+
+  return stringArray.join('\n');
+}
+
+var csv1 = "First,Last,Address\nAnnamarie,Romaguera,9446 Schroeder Squares\nSebastian,Hessel,114 Frederic Centers\nMarlon,Hahn,307 Bradtke Grove\nDuane,Schowalter,10771 Keaton Knoll\nLaurence,Schuster,610 Odell Point";
+console.log(csvToMd(csv1), '\n');
+var csv2 = "First,Last,Address,State\nAnnamarie,Romaguera,9446 Schroeder Squares,Colorado\nSebastian,Hessel,114 Frederic Centers,Oklahoma\nMarlon,Hahn,307 Bradtke Grove,New York\nDuane,Schowalter,10771 Keaton Knoll,Tennessee\nLaurence,Schuster,610 Odell Point,Texas";
+console.log(csvToMd(csv2));
