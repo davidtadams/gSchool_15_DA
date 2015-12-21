38591aede13dcf5742ab820069fec7c459bd2a82
davidtadams <adams.t.david@gmail.com>
2015-12-21 13:36:44 -0700

    added a psot route and router folder for separate routes

--- 2-term/4-week/1-day/student-fullstack/back-end-student-fullstack/index.js
+++ 2-term/4-week/1-day/student-fullstack/back-end-student-fullstack/index.js
@@ -1,53 +1,15 @@
 var express = require("express");
 var cors = require("cors");
-var knex = require("knex")({
-  client: "pg",
-  connection: {
-    host:         "127.0.0.1",
-    user:         "regly_app",
-    password:     "regly_password",
-    database:     "regly"
-  }
-});
+var bodyParser = require("body-parser");
+
+var studentsRoute = require("./routes/students");
 
 var app = express();
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-// var students = [{
-//   id: 1,
-//   first_name: "David",
-//   last_name: "Adams",
-//   date_of_birth: "02/19/1988",
-//   email: "myemail@gmail.com"
-// },{
-//   id: 2,
-//   first_name: "Kyle",
-//   last_name: "Coberly",
-//   date_of_birth: "01/01/2901",
-//   email: "danny@gmail.com"
-// },{
-//   id: 3,
-//   first_name: "CJ",
-//   last_name: "Reynolds",
-//   date_of_birth: "02/03/3000",
-//   email: "cj@gmail.com"
-// }];
-
-app.get("/students", function(request, response) {
-  knex("student").select().then(function(students) {
-    response.send({students: students});
-  });
-  // response.send({students: students});
-});
-
-app.get("/students/:id", function(request, response) {
-  knex("student").select()
-    .where("id", request.params.id)
-    .then(function(students) {
-      response.send({students: students});
-    });
-  // response.send({students: students[request.params.id - 1]});
-});
+app.use("/students", studentsRoute);
 
 app.listen(8000, function() {
   console.log("Runing on port: 8000");
---
+++ 2-term/4-week/1-day/student-fullstack/back-end-student-fullstack/routes/students.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var router = express.Router();
+
+var knex = require("knex")({
+  client: "pg",
+  connection: {
+    host:         "127.0.0.1",
+    user:         "regly_app",
+    password:     "regly_password",
+    database:     "regly"
+  }
+});
+
+router.get("/", function(request, response) {
+  knex("student").select().then(function(students) {
+    response.send({students: students});
+  });
+});
+
+router.get("/:id", function(request, response) {
+  knex("student").select()
+    .where("id", request.params.id)
+    .then(function(students) {
+      response.send({students: students});
+    });
+});
+
+router.post("/", function(request, response) {
+  knex("student").insert({
+    first_name: request.body.first_name,
+    last_name: request.body.last_name,
+    date_of_birth: request.body.date_of_birth,
+    email: request.body.email
+  }, "id").then(function(id) {
+    request.body.id = id[0];
+    response.status(201).send(request.body);
+  });
+});
+
+module.exports = router;
