09f9638ebce9154317ffb7e02a08c63bb6a26052
davidtadams <adams.t.david@gmail.com>
2015-12-21 10:13:29 -0700

    have simple server working

--- 2-term/4-week/1-day/student-fullstack/back-end-student-fullstack/index.js
+++ 2-term/4-week/1-day/student-fullstack/back-end-student-fullstack/index.js
@@ -1,1 +1,28 @@
 var express = require("express");
+var app = express();
+
+var students = [{
+  id: 1,
+  first_name: "David",
+  last_name: "Adams",
+  date_of_birth: "02/19/1988",
+  email: "myemail@gmail.com"
+},{
+  id: 2,
+  first_name: "Kyle",
+  last_name: "Coberly",
+  date_of_birth: "01/01/2901",
+  email: "danny@gmail.com"
+}];
+
+app.get("/students", function(request, response) {
+  response.send({students: students});
+});
+
+app.get("/students/:id", function(request, response) {
+  response.send({students: students[request.params.id - 1]});
+});
+
+app.listen(8000, function() {
+  console.log("Runing on port: 8000");
+});
