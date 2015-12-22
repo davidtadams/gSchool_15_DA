5d7646f768c2c39be37ddc724ec651f0eb31c2ab
davidtadams <adams.t.david@gmail.com>
2015-12-22 00:30:25 -0700

    reverting llama app.js back to original version

--- 2-term/3-week/5-day/server-exercise/app.js
+++ 2-term/3-week/5-day/server-exercise/app.js
@@ -1,32 +1,23 @@
 $(function () {
-  function setListeners() {
-    $('.kill').on('click', function (event) {
-      var id = $(this).attr('data')
-      $.ajax({
-        url: "http://localhost:8080/llamas/" + id,
-        method: "DELETE",
-      }).done(function (result) {
-        load();
-      })
-      .fail(function (err) {
-        $('.container').append('<h1 style="color: red;"> SERVER ERROR! CHECK THE CONSOLE </h1>')
-      })
-    })
+  (function load() {
+    $('.container').html('');
 
-    $('.rename').on('click', function (event) {
-      var id = $(this).attr('data')
-      var name = $('.nameBox.' + id).val()
-      $.ajax({
-        url: "http://localhost:8080/llamas/" + id,
-        method: "PUT",
-        data: {name: name}
-      }).done(function (result) {
-        load();
+    $.get('http://localhost:8080/llamas')
+      .done(function (llamas) {
+        llamas.forEach(function (llama) {
+          var name = '<input class="nameBox ' + llama.id + '" type="text" value="' + llama.name + '">'
+          var image = '<img class="llamaImage" src="' + llama.image + '" alt="' + llama.name + '">'
+          var renameButton = '<button data="' + llama.id + '"class="medium-4 columns rename success button" type="button">Rename</button>'
+          var killButton = '<button data="' + llama.id + '" class="medium-4 columns kill alert button" type="button">Kill</button>'
+          var open = '<div class="profile large-3 medium-3 columns">'
+          var close = '</div>'
+          $('.container').append(open + image + name + renameButton + killButton + close)
+        })
+        attachClickHandlers();
       })
       .fail(function (err) {
         $('.container').append('<h1 style="color: red;"> SERVER ERROR! CHECK THE CONSOLE </h1>')
       })
-    });
 
     $('.adopt').on('click', function (event) {
       var name = $('.name').val()
@@ -44,31 +35,36 @@ $(function () {
           })
       }
     })
-  }
 
-  function load() {
-    $('.container').html('');
-
-    $.get('http://localhost:8080/llamas')
-      .done(function (llamas) {
-
-        llamas.forEach(function (llama) {
-          var name = '<input class="nameBox ' + llama.id + '" type="text" value="' + llama.name + '">'
-          var image = '<img class="llamaImage" src="' + llama.image + '" alt="' + llama.name + '">'
-          var renameButton = '<button data="' + llama.id + '"class="medium-4 columns rename success button" type="button">Rename</button>'
-          var killButton = '<button data="' + llama.id + '" class="medium-4 columns kill alert button" type="button">Kill</button>'
-          var open = '<div class="profile large-3 medium-3 columns">'
-          var close = '</div>'
-          $('.container').append(open + image + name + renameButton + killButton + close)
-          setListeners();
+    function attachClickHandlers() {
+      $('.kill').on('click', function (event) {
+        var id = $(this).attr('data')
+        $.ajax({
+          url: "http://localhost:8080/llamas/" + id,
+          method: "DELETE",
+        }).done(function (result) {
+          load();
+        })
+        .fail(function (err) {
+          $('.container').append('<h1 style="color: red;"> SERVER ERROR! CHECK THE CONSOLE </h1>')
         })
       })
-      .fail(function (err) {
-        $('.container').append('<h1 style="color: red;"> SERVER ERROR! CHECK THE CONSOLE </h1>')
-      })
 
-  }
-
-  load();
+      $('.rename').on('click', function (event) {
+        var id = $(this).attr('data')
+        var name = $('.nameBox.' + id).val()
+        $.ajax({
+          url: "http://localhost:8080/llamas/" + id,
+          method: "PUT",
+          data: {name: name}
+        }).done(function (result) {
+          load();
+        })
+        .fail(function (err) {
+          $('.container').append('<h1 style="color: red;"> SERVER ERROR! CHECK THE CONSOLE </h1>')
+        })
+      });
+    }
+  })();
 
 })
