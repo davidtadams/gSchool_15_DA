b8b42b58cb1474eeb4ad41d5f2dcb9f4c73d224e
davidtadams <adams.t.david@gmail.com>
2015-12-22 09:15:39 -0700

    Finished llama server exercise

--- 2-term/3-week/5-day/server-exercise/app.js
+++ 2-term/3-week/5-day/server-exercise/app.js
@@ -1,9 +1,41 @@
 $(function () {
-  (function load() {
+  function setListeners() {
+    $('.kill').on('click', function (event) {
+      var id = $(this).attr('data')
+      $.ajax({
+        url: "http://localhost:8080/llamas/" + id,
+        method: "DELETE",
+      }).done(function (result) {
+        console.log('Loading file');
+        load();
+      })
+      .fail(function (err) {
+        $('.container').append('<h1 style="color: red;"> SERVER ERROR! CHECK THE CONSOLE </h1>')
+      })
+    })
+
+    $('.rename').on('click', function (event) {
+      var id = $(this).attr('data')
+      var name = $('.nameBox.' + id).val()
+      $.ajax({
+        url: "http://localhost:8080/llamas/" + id,
+        method: "PUT",
+        data: {name: name}
+      }).done(function (result) {
+        load();
+      })
+      .fail(function (err) {
+        $('.container').append('<h1 style="color: red;"> SERVER ERROR! CHECK THE CONSOLE </h1>')
+      })
+    });
+  }
+
+  function load() {
     $('.container').html('');
 
     $.get('http://localhost:8080/llamas')
       .done(function (llamas) {
+
         llamas.forEach(function (llama) {
           var name = '<input class="nameBox ' + llama.id + '" type="text" value="' + llama.name + '">'
           var image = '<img class="llamaImage" src="' + llama.image + '" alt="' + llama.name + '">'
@@ -13,58 +45,30 @@ $(function () {
           var close = '</div>'
           $('.container').append(open + image + name + renameButton + killButton + close)
         })
-        attachClickHandlers();
+
+        setListeners();
       })
       .fail(function (err) {
         $('.container').append('<h1 style="color: red;"> SERVER ERROR! CHECK THE CONSOLE </h1>')
       })
+  }
 
-    $('.adopt').on('click', function (event) {
-      var name = $('.name').val()
-      var image = $('.image').val()
-      if (name != "" && image != "") {
-        var data = {name: name, image: image}
-        $.post('http://localhost:8080/llamas', data)
-          .done(function (result) {
-            $('.name').val('')
-            $('.image').val('')
-            load();
-          })
-          .fail(function (err) {
-            $('.container').append('<h1 style="color: red;"> SERVER ERROR! CHECK THE CONSOLE </h1>')
-          })
-      }
-    })
-
-    function attachClickHandlers() {
-      $('.kill').on('click', function (event) {
-        var id = $(this).attr('data')
-        $.ajax({
-          url: "http://localhost:8080/llamas/" + id,
-          method: "DELETE",
-        }).done(function (result) {
+  $('.adopt').on('click', function (event) {
+    var name = $('.name').val()
+    var image = $('.image').val()
+    if (name != "" && image != "") {
+      var data = {name: name, image: image}
+      $.post('http://localhost:8080/llamas', data)
+        .done(function (result) {
+          $('.name').val('')
+          $('.image').val('')
           load();
         })
         .fail(function (err) {
           $('.container').append('<h1 style="color: red;"> SERVER ERROR! CHECK THE CONSOLE </h1>')
         })
-      })
-
-      $('.rename').on('click', function (event) {
-        var id = $(this).attr('data')
-        var name = $('.nameBox.' + id).val()
-        $.ajax({
-          url: "http://localhost:8080/llamas/" + id,
-          method: "PUT",
-          data: {name: name}
-        }).done(function (result) {
-          load();
-        })
-        .fail(function (err) {
-          $('.container').append('<h1 style="color: red;"> SERVER ERROR! CHECK THE CONSOLE </h1>')
-        })
-      });
     }
-  })();
+  })
 
+  load();
 })
