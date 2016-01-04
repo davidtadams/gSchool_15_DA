2a79e5edc8ac1b61d725990b2fed9fe4c43cb37e
davidtadams <adams.t.david@gmail.com>
2016-01-04 12:09:34 -0700

    adding changes to broncos answer

--- 2-term/5-week/1-day/broncosanswer/db/knex.js
+++ 2-term/5-week/1-day/broncosanswer/db/knex.js
@@ -1,4 +1,4 @@
-var environment = 'development';
+var environment = process.env.ENV || 'development';
 var config = require('../knexfile')[environment];
 var knex = require('knex')(config);
 module.exports = knex;
