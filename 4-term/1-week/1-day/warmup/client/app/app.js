f41ee800aa9d0e9715329df639b91efbcc4ddbb1
davidtadams <adams.t.david@gmail.com>
2016-02-22 12:23:38 -0700

    removing old warmup and adding finished version

--- 4-term/1-week/1-day/warmup/client/app/app.js
+++
@@ -1,9 +0,0 @@
-angular.module("realEstate", ['ui.router'])
-  .config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/home')
-    $stateProvider.state('home', {
-      templateUrl: 'templates/home.html',
-      controller: 'HomeController',
-      url: '/home'
-    })
-  })
