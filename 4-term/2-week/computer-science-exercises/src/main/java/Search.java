854e8cfbb1f9a5afaf24eb9458dc77d6261410ff
davidtadams <adams.t.david@gmail.com>
2016-03-03 11:42:18 -0700

    adding updates to cs exercises

--- 4-term/2-week/computer-science-exercises/src/main/java/Search.java
+++ 4-term/2-week/computer-science-exercises/src/main/java/Search.java
@@ -3,6 +3,12 @@ package main;
 import java.util.*;
 
 public class Search {
+  // Search functions will simply traverse a graph
+  // and return true if the value is found.
+  // Use a random starting node.
+  // The findNeighbors() method will be useful,
+  // make sure to complete the Graph exercise first.
+
   public static boolean dfs (String value, Graph graph) {
     return false;
   }
