3676d2c1ecb7c87da3dc9a1625ae94fc62cac89a
davidtadams <adams.t.david@gmail.com>
2016-03-03 23:02:01 -0700

    adding stack implementation

--- 4-term/2-week/computer-science-exercises/src/main/java/Stack.java
+++ 4-term/2-week/computer-science-exercises/src/main/java/Stack.java
@@ -1,20 +1,39 @@
 package main;
 
 public class Stack {
-  public void push(int value) {
+  private int size;
+  private int stackLength = 20;
+  private int[] stack = new int[stackLength];
 
+  public Stack() {
+    size = 0;
+  }
+
+  public void push(int value) {
+    stack[size] = value;
+    size += 1;
   }
 
   public int pop() {
-    return 0;
+    if (size == 0) {
+      throw new IllegalArgumentException("Empty Stack.");
+    }
+    size -= 1;
+    return stack[size];
   }
 
   public int peek() {
-    return 0;
+    if (size == 0) {
+      throw new IllegalArgumentException("Empty Stack.");
+    }
+    return stack[size - 1];
   }
 
   public boolean isEmpty() {
-    return false;
+    if (size > 0) {
+      return false;
+    }
+    return true;
   }
 
 }
